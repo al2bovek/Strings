31a9020cd74848885bbc94d0ee24da51cdc54e08
al2bovek <aleksandr.dubovek@gmail.com>
2025-11-23 00:03:24 +0200

    commit _1.js

--- _1.js
+++ _1.js
@@ -1,2 +1,14 @@
- "use strict";
+"use strict";
 // Remove Blanks: Create a function that, given a string, returns all of that string’s contents, but without blanks.
+const str = `Remove Blanks: Create a function that, given a string, returns all of that string's contents, but without blanks.`;
+
+const removeBlanks = (str) => {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+       if (str[i] !== " ") {
+        result += str[i];
+       } 
+    }
+    return result;
+}
+document.getElementById("string").innerHTML = removeBlanks(str);
